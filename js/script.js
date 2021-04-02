cff1f4e56b21e66ead557a84398a336a026fb797
ilarioni <ilia.meshvildishvili@gmail.com>
2021-04-02 13:11:22 +0200

    script
    hamburger menu open and close script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    }
+    else {
+        btnHamburger.classList.add('open');
+    }  
+});
