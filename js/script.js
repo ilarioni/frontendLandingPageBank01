3585f66f0ed40ec3280788ea45ca4b9537e25c5d
ilarioni <ilia.meshvildishvili@gmail.com>
2021-04-02 20:00:48 +0200

    animations
    fade-in fade-out
    @keyframes
    1:17 m

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){  // close hamburger menu
         header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     }
-    else {
+    else {  // open hamburger menu 
         header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
     }  
 });
