59fefd637f33d1c6c9ab515237ce2e9efbbf4ad6
ilarioni <ilia.meshvildishvili@gmail.com>
2021-04-04 16:44:30 +0200

    toggle
    script

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
     if(header.classList.contains('open')){  // close hamburger menu
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+        
     }
     else {  // open hamburger menu 
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+        
     }  
 });
