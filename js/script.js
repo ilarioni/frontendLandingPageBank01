b661a4da0542b0a4a0eea34f48c692117f8e32bb
ilarioni <ilia.meshvildishvili@gmail.com>
2021-04-04 16:54:36 +0200

    summing up
    part 3
    1:44

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
@@ -7,6 +8,7 @@ btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
     if(header.classList.contains('open')){  // close hamburger menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
         fadeElems.forEach(function(element){
             element.classList.remove('fade-in');
@@ -15,6 +17,7 @@ btnHamburger.addEventListener('click', function(){
         
     }
     else {  // open hamburger menu 
+        body.classList.add('noscroll');
         header.classList.add('open');
         fadeElems.forEach(function(element){
             element.classList.remove('fade-out');
